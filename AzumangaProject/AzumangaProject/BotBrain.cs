4897d942ba99dc918a68f5739d4e8d8afd076971
Ben Eshel <116272426+spooki2@users.noreply.github.com>
2024-04-10 01:22:14 +0300

    Add files via upload

---
+++ AzumangaProject/AzumangaProject/BotBrain.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AzumangaProject
+{
+    public static class BotBrain
+    {
+        public static Dictionary<Board, double> allChildBoards(Board board, Boolean botTurn, Boolean _ignoreMagi3_ = false,Boolean _respawnsCheck_ = false)
+        {
+            HashSet<Move> bothLegalMoves = BotFunctions.legalMoves(board, _ignoreMagi3_);
+            HashSet<Move> legalMoves = new HashSet<Move>();
+            foreach (Move move in bothLegalMoves)
+            {
+                if (botTurn)
+                {
+                    if (PieceManager.colorOnly(board.get1D(move.from)) == Piece.Black)
+                    {
+                        legalMoves.Add(move);
+                    }
+                }
+                else
+                {
+                    if (PieceManager.colorOnly(board.get1D(move.from)) == Piece.White)
+                    {
+                        legalMoves.Add(move);
+                    }
+                }
+            }
+
+
+            Dictionary<Board, double> boardScores = new Dictionary<Board, double>();
+            //make moves in child board
+
+            foreach (Move move in legalMoves)
+            {
+                //copy board
+                Board childBoard = new Board(board);
+                MovementManager.movePiece(move, childBoard);
+                double eval = BotFunctions.evaluate(childBoard);
+                boardScores[childBoard] = eval;
+            }
+
+            if (_respawnsCheck_==true)
+            {
+
+                //SLOW PART
+                //add respawn boards
+                HashSet<respawnMove> legalRespawns = BotFunctions.legalRespawns(board);
+                foreach (respawnMove move in legalRespawns)
+                {
+                    //copy board
+                    Board childBoard = new Board(board);
+                    MovementManager.placePiece(move.piece, move.to, childBoard);
+                    double eval = BotFunctions.evaluate(childBoard);
+                    boardScores[childBoard] = eval;
+                }
+
+
+            }
+
+            return boardScores;
+        }
+
+        //minimax
+
+        public static BoardScore minimax(Board currBoard, int depth, Boolean botTurn, Boolean _ignoreMagi3_ = false)
+        {
+            Dictionary<Board, double> childBoardScores = allChildBoards(currBoard, botTurn,_ignoreMagi3_,(depth==3));
+            if (depth == 0)
+            {
+                return new BoardScore(currBoard, BotFunctions.evaluate(currBoard));
+            }
+
+
+            if (botTurn)
+            {
+                BoardScore maxEval = new BoardScore(currBoard, int.MinValue);
+                foreach (Board childBoard in childBoardScores.Keys)
+                {
+                    BoardScore eval = minimax(childBoard, depth - 1, false, _ignoreMagi3_);
+                    if (eval.value > maxEval.value)
+                    {
+                        maxEval.value = eval.value;
+                        maxEval.board = childBoard;
+                    }
+                }
+
+                return maxEval;
+            }
+            else
+            {
+                BoardScore minEval = new BoardScore(currBoard, int.MaxValue);
+                foreach (Board childBoard in childBoardScores.Keys)
+                {
+                    BoardScore eval = minimax(childBoard, depth - 1, true, _ignoreMagi3_);
+                    if (eval.value < minEval.value)
+                    {
+                        minEval.value = eval.value;
+                        minEval.board = childBoard;
+                    }
+                }
+
+                return minEval;
+            }
+        }
+    }
+}
---
+++ AzumangaProject/AzumangaProject/BotFunctions.cs
@@ -0,0 +1,143 @@
+﻿namespace AzumangaProject;
+
+public static class BotFunctions
+{
+    public static double evaluate(Board board)
+    {
+        double evalScore = 0;
+        HashSet<int> pieces = new HashSet<int>(getPieces(board));
+        foreach (int invp in board.Inv)
+        {
+            pieces.Add(invp);
+        }
+
+        foreach (int piece in pieces)
+        {
+            if (PieceManager.colorOnly(piece) == Piece.Black)
+            {
+                evalScore += PointSystem.pieceValue[(PieceManager.pieceOnly(piece))];
+            }
+            else
+            {
+                evalScore -= PointSystem.pieceValue[(PieceManager.pieceOnly(piece))];
+            }
+        }
+
+
+        if (!pieces.Contains(Piece.King | Piece.Black))
+        {
+            evalScore += -10000;
+        }
+        if (!pieces.Contains(Piece.King | Piece.White))
+        {
+            evalScore += 1000;
+        }
+
+
+        //adjusted mate bonus should be between -3 and 3
+        double adjustedMateBonus = Math.Min(3, Math.Max(-3, mateBonus(board) / 10));
+
+
+        return evalScore + adjustedMateBonus;
+    }
+
+
+    public static double mateBonus(Board board)
+    {
+        //encourage opponent king to be furthest from center
+        double mateBonus = 0;
+        int kingPos1D = 0;
+        int[] whiteSumPos = [0, 0];
+        int counterWhite = 0;
+        int[] centerPos = [3, 3];
+        for (int i = 0; i < 36; i++)
+        {
+            if (board.get1D(i) == (Piece.King | Piece.Black))
+            {
+                kingPos1D = i;
+            }
+
+            if (PieceManager.colorOnly(board.get1D(i)) == Piece.White&&PieceManager.pieceOnly(board.get1D(i))!=Piece.King)
+            {
+                whiteSumPos[0] += i % 6;
+                whiteSumPos[1] += i / 6;
+                counterWhite++;
+            }
+        }
+
+        double[] whiteAveragePos = [whiteSumPos[0] / counterWhite, whiteSumPos[1] / counterWhite];
+
+
+        int[] kingPos = [kingPos1D % 6, kingPos1D / 6];
+        double kingCenterDistance = Math.Abs(centerPos[0] - kingPos[0]) + Math.Abs(centerPos[1] - kingPos[1]);
+        double whiteKingDistance =
+            Math.Abs(whiteAveragePos[0] - kingPos[0]) + Math.Abs(whiteAveragePos[1] - kingPos[1]);
+        return kingCenterDistance + whiteKingDistance;
+    }
+
+
+    public static List<int> getPieces(Board board)
+    {
+        List<int> pieces = new List<int>();
+        for (int i = 0; i < 36; i++)
+        {
+            int piece = board.get1D(i);
+            if (piece != Piece.None)
+            {
+                pieces.Add(piece);
+            }
+        }
+
+        return pieces;
+    }
+
+    public static HashSet<respawnMove> legalRespawns(Board board)
+    {
+        HashSet<respawnMove> legalRespawns = new HashSet<respawnMove>();
+
+
+        List<int> capturedPieces = board.Inv;
+        foreach (int piece in capturedPieces)
+        {
+            for (int i = 0; i < 36; i++)
+            {
+                if (MovementManager.isRespawnLegal(piece, i, board))
+                {
+                    respawnMove move = new respawnMove(piece, i);
+                    legalRespawns.Add(move);
+                }
+            }
+        }
+
+        return legalRespawns;
+    }
+
+    public static HashSet<Move> legalMoves(Board board, Boolean _ignoreMagi3_ = false)
+    {
+        List<int> pieces = getPieces(board);
+        List<int> positions = new List<int>();
+        for (int i = 0; i < 36; i++)
+        {
+            if (board.get1D(i) != Piece.None)
+            {
+                positions.Add(i);
+            }
+        }
+
+        HashSet<Move> legalMoves = new HashSet<Move>();
+        foreach (int from in positions)
+        {
+            int piece = board.get1D(from);
+            for (int i = 0; i < 36; i++)
+            {
+                Move move = new Move(from, i);
+                if (MovementManager.isMegaLegal(piece, move, board, _ignoreMagi3_))
+                {
+                    legalMoves.Add(move);
+                }
+            }
+        }
+
+        return legalMoves;
+    }
+}
